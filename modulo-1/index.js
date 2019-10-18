5f66c50d1a4d642a08dabed51c1042513097d760
marcosvanille <marcosvanillle@gmail.com>
2019-10-17 22:35:22 -0300

    [UPDATE]brincando com rotas no react js

--- modulo-1/index.js
+++ modulo-1/index.js
@@ -1,33 +1,60 @@
 const express = require('express');
 
 const server = express();
+server.use(express.json()); //usae para o express entender o json 
 
 // localhost:3000/teste
 
 // 3 tipos de parametros 
 //Query params = ?teste=1  formato com ponto de interrogaçao
 // server.get('/teste' , (req , res) => {
-// const nome = req.query.nome;
+// const nome = req.query.nome; 
 //  return res.json({message: `hello ${nome}`});
 // })
 
-// Route params = /users/1  parametro com id 
+// Route params = /users/1  parametro com id, quando  UTILIZAR O : na requisiçao na url significa que vem um id por parametro
 // server.get('/users/:id' , (req , res) => {
 //     const {id} = req.params;
 //      return res.json({message: `Buscando um usuario ${id}`});
 //     })
 
 //Request body = {"marcos","email: "marcos@rocketseat.com.br"} 
-
-const users = ['diego', 'vanille','victor']; // variavel
+const users = ['diego', 'vanille','victor']; // variavel 
  
-server.get('/users/:index' , (req , res) => {
-    const {index} = req.params;
+//users esta listando todos
+server.get('/users/' , (req , res) => {  
+ 
+     return res.json(users);
+})
+
+//:index pegar a requisiçao por id
+server.get('/users/:index' , (req , res) => { 
+    const {index} = req.params;  
+
      return res.json(users[index]);
+
     })
 
 
 
+server.post('/users', (req,res) =>{
+const {name} = req.body;
+
+users.push(name);
+
+return res.json(users);
+});
+
+server.put('/users/:index' , (req,res) =>{
+
+    const {index} = req.params;
+    const {name} = req.body;
+
+    users[index] = name;
+
+    return res.json(users);
+
+});
 
 
 server.listen(3000);
